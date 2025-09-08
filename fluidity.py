880e6696236f258f7a3234e53960bc5f12f32eea
Ned Batchelder <ned@nedbatchelder.com>
2025-09-08 06:41:31 -0400

    works

---
+++ fluidity.py
@@ -0,0 +1,109 @@
+from drawing import cairo_context
+
+import super_simplex
+import numpy as np
+from hilbertcurve.hilbertcurve import HilbertCurve
+from hobby import HobbyCurve
+
+
+def hobby_points(points):
+    curve = HobbyCurve(points, cyclic=True)
+    return curve.get_ctrl_points()
+
+
+def draw_lines(ctx, points, closed=True):
+    ctx.move_to(*points[0])
+    for pt in points[1:]:
+        ctx.line_to(*pt)
+    if closed:
+        ctx.close_path()
+    ctx.stroke()
+
+
+def draw_hobby(ctx, points):
+    numpt = len(points)
+    ctrls = hobby_points(points)
+    ctx.move_to(*points[0])
+    for i in range(numpt):
+        ctx.curve_to(*ctrls[2 * i], *ctrls[2 * i + 1], *points[(i + 1) % numpt])
+    ctx.stroke()
+
+
+class HilbertSorter:
+    def choose_order(self, points):
+        P = 6  # 2^6 = 64 resolution per dimension
+        hilbert_curve = HilbertCurve(P, 2)
+        max_coord = 2**P - 1
+        pointsa = np.array(points)
+        min_vals = pointsa.min(axis=0)
+        max_vals = pointsa.max(axis=0)
+
+        normalized_points = (
+            (pointsa - min_vals) / (max_vals - min_vals) * max_coord
+        ).astype(int)
+
+        hilbert_distances = hilbert_curve.distances_from_points(
+            normalized_points.tolist()
+        )
+        self.sorted_indices = np.argsort(hilbert_distances)
+
+    def sort(self, points):
+        pointsa = np.array(points)
+        sorted_points = pointsa[self.sorted_indices]
+        return sorted_points
+
+
+class Fluidity:
+    def __init__(
+        self,
+        seed: int = 17,
+        npoints: int = 10,
+        nlines: int = 100,
+        ystep: float = 0.002,
+    ):
+        self.gen_simplex = super_simplex.Gener([seed])
+
+        self.lines = [
+            [
+                (
+                    self.simplex(i * 17.5 + 1, j * ystep + 0.001) * 300 + 300,
+                    self.simplex(i * 17.5 + 2, j * ystep + 0.002) * 300 + 300,
+                )
+                for i in range(npoints)
+            ]
+            for j in range(nlines)
+        ]
+
+    def simplex(self, x, y):
+        return self.gen_simplex.noise_2d(x, y)[0]
+
+    def draw(
+        self,
+        line_color=(1, 0.2, 0.2, 0.3),
+        curve_color=(0, 0, 0, 0.3),
+        one_order=False,
+    ):
+        sorter = HilbertSorter()
+        if one_order:
+            sorter.choose_order(self.lines[0])
+        with cairo_context(600, 600, format="svg") as context:
+            for line in self.lines:
+                if not one_order:
+                    sorter.choose_order(line)
+                line = sorter.sort(line)
+                if line_color is not None:
+                    context.set_source_rgba(*line_color)
+                    context.set_line_width(0.05)
+                    draw_lines(context, line)
+                if curve_color is not None:
+                    context.set_source_rgba(*curve_color)
+                    context.set_line_width(0.25)
+                    draw_hobby(context, line)
+        return context
+
+    def draw_points(self):
+        with cairo_context(600, 600, format="svg") as context:
+            context.set_line_width(0.5)
+            for line in zip(*self.lines):
+                draw_lines(context, line, closed=False)
+        return context
