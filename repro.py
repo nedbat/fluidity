a5500a62c71d7be75558ecb5b3858086ea765203
Ned Batchelder <ned@nedbatchelder.com>
2025-09-25 07:34:19 -0400

    pick one repro

--- repro.py
+++ repro.py
@@ -1,17 +1,27 @@
 from drawing import *
 from fluidity import *
 
-for seed in range(50):
-    with cairo_context(600, 600, format="png", output=f"repro/{seed:02d}.png") as context:
-        Fluidity(
-            LinearNoise(seed=seed, istep=0.001, istart=0.07),
-            npoints=10, nlines=40, curver=hobby_curve, sorter=HilbertSortEveryLine()
-        ).draw_in_context(
-            context,
-            curve_color=(0, 0, 0, 0.3),
-            curve_width=1,
-        )
+if 0:
+    for seed in range(100):
+        with cairo_context(600, 600, format="png", output=f"repro/{seed:02d}.png") as context:
+            Fluidity(
+                LinearNoise(seed=seed+100, istep=0.001, istart=0.07),
+                npoints=10, nlines=40, curver=hobby_curve, sorter=HilbertSortEveryLine()
+            ).draw_in_context(
+                context,
+                curve_color=(0, 0, 0, 0.3),
+                curve_width=1.5,
+            )
 
+with cairo_context(600, 600, format="png", output=f"pix/repro_139.png") as context:
+    Fluidity(
+        LinearNoise(seed=139, istep=0.001, istart=0.07),
+        npoints=10, nlines=40, curver=hobby_curve, sorter=HilbertSortEveryLine()
+    ).draw_in_context(
+        context,
+        curve_color=(0, 0, 0, 0.3),
+        curve_width=1.5,
+    )
 
 with cairo_context(400, 400, format="png", output=f"pix/point_motion.png") as context:
     f = Fluidity(LinearNoise(seed=7, istep=0.03), npoints=4, nlines=50)
