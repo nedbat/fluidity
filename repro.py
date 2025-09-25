eac2bf31ece1f11748570f68dfa5340a4914aeb6
Ned Batchelder <ned@nedbatchelder.com>
2025-09-25 06:28:44 -0400

    point_motion.png

--- repro.py
+++ repro.py
@@ -1,7 +1,7 @@
 from drawing import *
 from fluidity import *
 
-for seed in range(100):
+for seed in range(50):
     with cairo_context(600, 600, format="png", output=f"repro/{seed:02d}.png") as context:
         Fluidity(
             LinearNoise(seed=seed, istep=0.001, istart=0.07),
@@ -10,5 +10,10 @@ for seed in range(100):
             context,
             size=(600, 600),
             curve_color=(0, 0, 0, 0.3),
-            curve_size=1,
+            curve_width=1,
         )
+
+
+with cairo_context(400, 400, format="png", output=f"pix/point_motion.png") as context:
+    f = Fluidity(LinearNoise(seed=7, istep=0.03), npoints=4, nlines=50)
+    f.draw_in_context(context, size=(400,400), curve_color=None, point_color=(1, 0, 0, 1), point_size=1.5)
